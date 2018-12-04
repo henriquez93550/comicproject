69c9c0706112be13cd88700665ab5457acb16b84
Ben-Massey <pojoben@gmail.com>
2018-12-04 00:49:36 -0600

    setup everything for the database other than the form to pull in the inputs

--- models/example.js
+++ models/example.js
@@ -1,6 +1,45 @@
 module.exports = function(sequelize, DataTypes) {
   var submissions = sequelize.define("submissions", {
-    word: DataTypes.STRING,
+    w1: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    w2: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    w3: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    w4: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    w5: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    w6: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    w7: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    w8: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    w9: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    w10: {
+      type: DataTypes.STRING,
+      required: true
+    }
   });
   return submissions;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,15 +3,26 @@ var db = require("../models");
 module.exports = function(app) {
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.words.create(req.body).then(function(wordsdb) {
+  app.post("/api/words", function(req, res) {
+    db.words.create({
+      w1: req.body.word1,
+      w2: req.body.word2,
+      w3: req.body.word3,
+      w4: req.body.word4,
+      w5: req.body.word5,
+      w6: req.body.word6,
+      w7: req.body.word7,
+      w8: req.body.word8,
+      w9: req.body.word9,
+      w10: req.body.word10
+    }).then(function(wordsdb) {
       res.json(wordsdb);
     });
   });
 
     // Get all examples
-    app.get("/api/examples", function(req, res) {
-      db.Example.findAll({}).then(function(wordsdb) {
+    app.get("/api/words", function(req, res) {
+      db.words.findAll({}).then(function(wordsdb) {
         res.json(wordsdb);
       });
     });
