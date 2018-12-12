65d3f52a66cbb011cf2e5029aa1997bf9a87ba59
HurSpar <brianrice2382@gmail.com>
2018-12-11 22:25:25 -0600

    update to buttons updates all percents each buutton press

--- public/js/index.js
+++ public/js/index.js
@@ -63,29 +63,19 @@ $(function () {
 		if (userVote === "1") {
 			vote1Counter++;
 			totalCounter++;
-			sameChoice1 = Math.floor(vote1Counter / totalCounter * 100);
-			$(".ratings1").html(sameChoice1 + "% of users have chosen the Forunate Events story.");
-			$(".ratings2").html(sameChoice2 + "% of users have chosen the Motor Skills story.");
-			$(".ratings3").html(sameChoice3 + "% of users have chosen the Furry Friends story.");
-	
+			updateVotes();
 		}
 	
 		else if (userVote === "2") {
 			vote2Counter++;
 			totalCounter++;
-			sameChoice2 = Math.floor(vote2Counter / totalCounter * 100);
-			$(".ratings2").html(sameChoice2 + "% of users have chosen the Motor Skills story.");
-			$(".ratings3").html(sameChoice3 + "% of users have chosen the Furry Friends story.");
-			$(".ratings1").html(sameChoice1 + "% of users have chosen the Forunate Events story.");
+			updateVotes();
 		}
 	
 		else {
 			vote3Counter++;
 			totalCounter++;
-			sameChoice3 = Math.floor(vote3Counter / totalCounter * 100);
-			$(".ratings3").html(sameChoice3 + "% of users have chosen the Furry Friends story.");
-			$(".ratings2").html(sameChoice2 + "% of users have chosen the Motor Skills story.");
-			$(".ratings1").html(sameChoice1 + "% of users have chosen the Forunate Events story.");
+			updateVotes();
 		}
 		database.ref().set({
 			vote1Count: vote1Counter,
@@ -104,6 +94,16 @@ $(function () {
 	
 	});
 
+	function updateVotes(){
+		sameChoice1 = Math.floor(vote1Counter / totalCounter * 100);
+		$(".ratings1").html(sameChoice1 + "% of users have chosen the Forunate Events story.");
+		sameChoice2 = Math.floor(vote2Counter / totalCounter * 100);
+		$(".ratings2").html(sameChoice2 + "% of users have chosen the Motor Skills story.");
+		sameChoice3 = Math.floor(vote3Counter / totalCounter * 100);
+		$(".ratings3").html(sameChoice3 + "% of users have chosen the Furry Friends story.");
+	
+	}
+
 });
 
 
@@ -114,80 +114,4 @@ $exampleList.on("click", ".delete", handleDeleteBtnClick);
 
 
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCgw7HqOw9nbPA6cjRTrsQQcn0tlDa12aE",
-    authDomain: "madlib-68ea6.firebaseapp.com",
-    databaseURL: "https://madlib-68ea6.firebaseio.com",
-    projectId: "madlib-68ea6",
-    storageBucket: "madlib-68ea6.appspot.com",
-    messagingSenderId: "639479628767"
-  };
-  firebase.initializeApp(config);
-
-var database = firebase.database();
-var initialValue = 0;
-var vote1Counter = initialValue;
-var vote2Counter= initialValue;
-var vote3Counter = initialValue;
-var totalCounter = initialValue;
-var userVote = initialValue;
-
-database.ref().on("value", function(snapshot){
-console.log(snapshot.val());
-
- vote1Counter = snapshot.val().vote1Count;
- vote2Counter= snapshot.val().vote2Count;
- vote3Counter = snapshot.val().vote3Count;
- totalCounter = snapshot.val().totalCount;
-
- console.log(vote1Counter);
- console.log(vote2Counter);
- console.log(vote3Counter);
- console.log(totalCounter);
-});
-
-
-
-$(".rate").on("click", function(){
-  event.preventDefault();
- var userVote = this.value;
- console.log(userVote);
-
-  if (userVote === "1") {
-    vote1Counter++;
-    totalCounter++;
-    sameChoice1 = Math.floor(vote1Counter / totalCounter * 100);
-    $(".ratings1").html(sameChoice1 + "% of users have chosen the Forunate Events story.")
-  } 
-
-  else if( userVote === "2"){
-    vote2Counter++;
-    totalCounter++;
-    sameChoice2 = Math.floor(vote2Counter / totalCounter * 100);
-    $(".ratings2").html(sameChoice2 + "% of users have chosen the Motor Skills story.")
-    }
-
-    else {
-      vote3Counter++;
-    totalCounter++;
-    sameChoice3 = Math.floor(vote3Counter / totalCounter * 100);
-    $(".ratings3").html(sameChoice3 + "% of users have chosen the Furry Friends story.")
-    }
-    database.ref().set({
-      vote1Count: vote1Counter,
-      vote2Count: vote2Counter,
-      vote3Count: vote3Counter,
-      totalCount: totalCounter
-    });
-
-    database.ref().set({
-      vote1Count: vote1Counter,
-      vote2Count: vote2Counter,
-      vote3Count: vote3Counter,
-      totalCount: totalCounter
-    });    
-console.log(sameChoice);
-
-});
-
+  
